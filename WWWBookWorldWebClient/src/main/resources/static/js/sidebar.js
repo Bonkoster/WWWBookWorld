2447a05d5b212b59ef9ddf6febbf2c9ef045f71c
Lukashman <shram3214@gmail.com>
2019-06-03 21:42:39 +0300

    Added jquery UI

--- WWWBookWorldWebClient/src/main/resources/static/js/sidebar.js
+++ WWWBookWorldWebClient/src/main/resources/static/js/sidebar.js
@@ -1,12 +1,10 @@
 require(['jquery'], function($) {
-	$(".closeBtn").click( 
-			() => {
+	$(".closeBtn").click( () => {
 				$(".openBtn").show();
 				$(".sidebar").width(0);
 				$(".main-container").width("100%");
 			});
-	$(".openBtn").click( 
-			() => {
+	$(".openBtn").click( () => {
 				$(".openBtn").hide();
 				$(".sidebar").width("250px");
 				$(".main-container").width("100%").width("-=250px");
